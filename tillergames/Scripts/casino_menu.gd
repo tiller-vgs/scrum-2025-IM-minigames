d1e3f07cc865ac7dad58f9e784c805fe4e05f6bc
user <tirian.druett.sjaastadgmail.com>
2025-05-08 08:39:37 +0200

    removed casion files

--- tillergames/Scripts/casino_menu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_blackjack_pressed():
-	get_tree().change_scene_to_file("res://Scenes/blackjack.tscn")
-
-func _on_leave_pressed():
-	get_tree().change_scene_to_file("res://Scenes/minimap_menu.tscn")
