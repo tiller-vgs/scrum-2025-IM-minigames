d45fe39ff7171618c828649c7dcc9e3aa008d48c
user <tirian.druett.sjaastadgmail.com>
2025-05-08 11:20:49 +0200

    yay

--- tillergames/Scripts/minimap_menu.gd
+++ tillergames/Scripts/minimap_menu.gd
@@ -5,7 +5,9 @@ extends Control
 func _on_casino_pressed():
 	get_tree().change_scene_to_file("res://Scenes/casino.tscn")
 
-
-
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes/start_menu.tscn")
+
+
+func _on_war_room_pressed():
+	get_tree().change_scene_to_file("res://Scenes/warroom.tscn")
